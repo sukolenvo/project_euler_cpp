469a1e90d99313e55f243c78559fb5166d356d58
vadym <vadym.sukolen@staff.wisetime.com>
2019-12-12 21:04:14 +0800

    problem002.cpp

---
+++ problem002.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    long long prev {1};
+    long long curr {1};
+    long long result {};
+    while (true) {
+        long long next = prev + curr;
+        if (next > 4'000'000) {
+            break;
+        }
+        if (!(next & 1)) {
+            result += next;
+        }
+        prev = curr;
+        curr = next;
+    }
+    std::cout << result;
+    return 0;
+}
